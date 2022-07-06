a0d59d0f342c25edc6669f41f4211f877766e3e7
Inigo Peng <34848367+InigoP@users.noreply.github.com>
2022-07-06 16:02:55 -0700

    formatting hydrolab excel into wqx format

--- data-raw/clean_hydrolab_data.R
+++ data-raw/clean_hydrolab_data.R
@@ -17,18 +17,21 @@ location_id <- unlist(
     read_csv('data-raw/FC1.csv', col_names = FALSE, n_max = 1),
     regex_pattern))
 
-unit_lookup <- tibble("Characteristic Name" = c("Temperature, water", "Specific conductance", "Resistivity", "Salinity", "Totla dissolved solids", "Dissolved oxygen saturation","Dissolved oxygen (DO)", "pH", "Turbidity"),
+unit_lookup <- tibble("Characteristic Name" = c("Temperature, water", "Specific conductance", "Resistivity", "Salinity", "Total dissolved solids", "Dissolved oxygen saturation","Dissolved oxygen (DO)", "pH", "Turbidity"),
                       "Result Unit" = c("deg C", "mS/cm", "KOhm/cm", "ppt", "g/L", "%", "mg/L", "None","NTU"))
+# project_id_lookup
 
-make_activity_id <- function(location_id, date, activity_type, equipment_name, time = NULL) {
+
+make_activity_id <- function(location_id, date, activity_type, equipment_name, depth = NULL, time = NULL) {
   YYYYMMDD <- gsub('/', '', date)
   activity <- ifelse(activity_type == "Sample-Routine", "SR", "FM")
   equipment <- ifelse(equipment_name == "Probe/Sensor", "PS", NA)
   hhmm <- gsub(':', '', time)
-  paste(location_id, YYYYMMDD, hhmm,activity, equipment, sep = ":")
+  equipment_comment <- ifelse(equipment_name == "Probe/Sensor", "Hydro", NA)
+  paste(location_id, YYYYMMDD, hhmm,activity, equipment, depth, equipment_comment, sep = ":")
 }
-
-cleaned_data <- raw_data %>% 
+# ------------------------------------------------------------------------------
+formatted_for_wqx <- raw_data %>% 
   select(-c(3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29)) %>% 
   filter(!row_number() %in% c(1,3)) %>% 
   clean_names() %>%
@@ -44,16 +47,16 @@ cleaned_data <- raw_data %>%
          "Turbidity" = "turb") %>% 
     pivot_longer(!c(date, time, depth10), names_to = "Characteristic Name", "values_to" = "Result Value") %>%  
   left_join(unit_lookup) %>%
-  mutate("Project ID" = NA,
+  mutate("Project ID" = "",
          "Monitoring Location ID" = location_id,
          # "Activity ID (CHILD-subset)" = make_activity_id(location_id, date, activity_type, equipment_name, time = NULL), 
-         "Activity ID User Supplied(PARENTs)" = NA,
+         "Activity ID User Supplied(PARENTs)" = "",
          "Activity Type" = "Field Msr/Obs",
          "Activity Media Name" = "Water",
          "Activity Start Date" = format(mdy(date), "%m/%d/%Y"),
-         "Activity Start Time" = format(hm(time), "%I%M"),
+         "Activity Start Time" = format(parse_date_time(time, c('HMS', 'HM')), "%H:%M"),
          "Activity Start Time Zone" = "PST",
-         "Activity Depth/Height Measure" = depth10,
+         "Activity Depth/Height Measure" = as.numeric(depth10),
          "Activity Depth/Height Unit" = "m",
          "Sample Collection Method ID" = "BVR Tribal SWQAPP",
          "Sample Collection Method Context" = "CA_BVR",
@@ -61,33 +64,70 @@ cleaned_data <- raw_data %>%
          "Sample Collection Equipment Comment" = "Hydrolab® Surveyor DS5 Multiprobe 
 ",
          "Characteristic Name" = `Characteristic Name`,
-         "Characteristic Name User Supplied" = NA,
-         "Method Speciation" = NA,
-         "Result Detection Condition" = NA,
+         "Characteristic Name User Supplied" = "",
+         "Method Speciation" = "",
+         "Result Detection Condition" = "",
          "Result Value" = `Result Value`,
          "Result Unit" = `Result Unit`,
-         "Result Measure Qualifier" = NA,
-         "Result Sample Fraction" = NA,
+         "Result Measure Qualifier" = "",
+         "Result Sample Fraction" = "",
          "Result Status ID" = "Final",
-         "ResultTemperatureBasis" = NA,
-         "Statistical Base Code" = NA,
-         "ResultTimeBasis" = NA,
+         "ResultTemperatureBasis" = "",
+         "Statistical Base Code" = "",
+         "ResultTimeBasis" = "",
          "Result Value Type" = "Actual",
-         "Activity ID" = make_activity_id(location_id = `Monitoring Location ID`,
+         "Activity ID (CHILD-subset)" = make_activity_id(location_id = `Monitoring Location ID`,
                                           date = `Activity Start Date`,
                                           time = `Activity Start Time`,
                                           activity_type = `Activity Type`,
-                                          equipment_name = `Sample Collection Equipment Name`),
-         "Result Analytical Method ID" = NA,
-         "Result Analytical Method Context" = NA,
-         "Analysis Start Date" = NA,
-         "Result Detection/Quantitation Limit Type" = NA,
-         "Result Detection/Quantitation Limit Measure" = NA,
-         "Result Detection/Quantitation Limit Unit" = NA,
-         "Result Comment" = NA
+                                          equipment_name = `Sample Collection Equipment Name`,
+                                          depth = `Activity Depth/Height Measure`),
+         "Result Analytical Method ID" = "",
+         "Result Analytical Method Context" = "",
+         "Analysis Start Date" = "",
+         "Result Detection/Quantitation Limit Type" = "",
+         "Result Detection/Quantitation Limit Measure" = "",
+         "Result Detection/Quantitation Limit Unit" = "",
+         "Result Comment" = ""
          
          ) %>% 
   select(-c(date, depth10, time)) %>% 
-  glimpse
-  # rename("Activity Start Date") )
+  relocate("Project ID",
+           "Monitoring Location ID",
+           "Activity ID (CHILD-subset)",
+           "Activity ID User Supplied(PARENTs)",
+           "Activity Type",
+           "Activity Media Name",
+           "Activity Start Date",
+           "Activity Start Time",
+           "Activity Start Time Zone",
+           "Activity Depth/Height Measure",
+           "Activity Depth/Height Unit",
+           "Sample Collection Method ID",
+           "Sample Collection Method Context",
+           "Sample Collection Equipment Name",
+           "Sample Collection Equipment Comment",
+           "Characteristic Name",
+           "Characteristic Name User Supplied",
+           "Method Speciation",
+           "Result Detection Condition",
+           "Result Value",
+           "Result Unit",
+           "Result Measure Qualifier",
+           "Result Sample Fraction",
+           "Result Status ID",
+           "ResultTemperatureBasis",
+           "Statistical Base Code",
+           "ResultTimeBasis",
+           "Result Value Type",
+           "Result Analytical Method ID",
+           "Result Analytical Method Context",
+           "Analysis Start Date",
+           "Result Detection/Quantitation Limit Measure",
+           "Result Detection/Quantitation Limit Unit",
+           "Result Comment"
+           )
+
+write_csv(formatted_for_wqx, "data/hydrolab_wqx.csv")
+
   
